472afdad7a83c2e049079082af84d7e555a8bd02
Duyet Nguyen <duyet.vn@gmail.com>
2016-09-21 15:44:03 +0700

    show details commodity

--- app/controllers/commodities_controller.rb
+++ app/controllers/commodities_controller.rb
@@ -1,6 +1,8 @@
 class CommoditiesController < ApplicationController
+  before_action :set_commodity, only: [:show]
+
   def index
-    @commodities = Commodity.all
+    @commodities = Commodity.available.order("created_at desc")
     @users       = User.all
 
     respond_to do |format|
@@ -12,6 +14,15 @@ class CommoditiesController < ApplicationController
         }
       }
     end
+  end
 
+  def show
+
+  end
+
+  private
+
+  def set_commodity
+    @commodity = Commodity.find_by(params[:id])
   end
 end
--- app/models/commodity.rb
+++ app/models/commodity.rb
@@ -1,5 +1,7 @@
 class Commodity < ApplicationRecord
   belongs_to :shop_owner, class_name: "User"
   belongs_to :shipper, class_name: "User"
+
+  scope :available, -> {where picked: false}
 end
 
