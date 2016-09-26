30fa7b5fa3a678cffe9e2a9a6e14e412b38339e8
Duyet Nguyen <duyet.vn@gmail.com>
2016-09-26 13:49:00 +0700

    add button clear filter

--- app/assets/javascripts/components/commodities/commodities.es6.jsx
+++ app/assets/javascripts/components/commodities/commodities.es6.jsx
@@ -51,6 +51,25 @@ class Commodities extends Component {
       });
     }
 
+  handleResetFilter(e){
+    $.ajax({
+      url: "/commodities",
+      dataType: "json",
+      data: {tag: ""},
+      error: function (xhr, status, error) {
+        console.log('AJAX Error: ' + status + error);
+      },
+      success: function (response) {
+        this.setState({commodities: response["commodities"]});
+        this.setState({users: response["users"]});
+        this.setState({current_user: response["current_user"]});
+        this.setState({bids: response["bids"]});
+        this.setState({didFetchData: true});
+      }.bind(this)
+    });
+    e.preventDefault();
+  }
+
     render() {
         if(this.state.users.length > 0 && this.state.commodities.length > 0 && this.state.didFetchData){
             var cardsCommodity = this.state.commodities.map((commodity) => {
@@ -70,6 +89,9 @@ class Commodities extends Component {
 
         return (
             <div id="commodity-index" className="col-xs-12">
+                <div className="clear-filter pull-right">
+                  <a href="#" onClick={this.handleResetFilter.bind(this)}><i className="fa fa-eraser" aria-hidden="true"></i> Clear filter</a>
+                </div>
                 {cardsCommodity}
             </div>
         );
